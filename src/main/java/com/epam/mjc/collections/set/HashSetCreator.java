35e8f826aba949e7fed606ca5ff9e2686a54244f
iaveltauri <veltauriia@gmail.com>
2022-11-30 23:12:58 +0400

    Init commit

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet <Integer> res = new HashSet<>();
+        for (int i : sourceList){
+            if(i%2 == 0){
+                while (i%2 == 0){
+                    res.add(i);
+                    i=i/2;
+                }
+                res.add(i);
+            }else {
+                res.add(i);
+                res.add(2*i);
+            }
+        }
+
+        return res;
     }
 }
+
